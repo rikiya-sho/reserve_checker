c51439c59098435b8dd750d999160728794fcbc8
Rikiya Shogen <iphone.shogen@gmail.com>
2020-07-15 01:09:36 +0000

    Heroku対応修正1

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,8 +1,14 @@
 'use strict';
 const Sequelize = require('sequelize');
+//DBローカル環境設定
+/*
 const sequelize = new Sequelize(
   'postgres://postgres:postgres@localhost/reserve_checker'
-);
+);*/
+//DB heroku設定
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/reserve_checker'
+ );
 
 module.exports = {
   database: sequelize,
