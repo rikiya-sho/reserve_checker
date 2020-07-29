14d706f4161af64dcaba1f096fb6b74ebe616ed5
Rikiya Shogen <iphone.shogen@gmail.com>
2020-07-29 10:29:50 +0000

    認証修正

--- app.js
+++ app.js
@@ -34,12 +34,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //ベーシック認証
 //全体に適用
-app.use(basicAuth('master', 'edit'));
+//app.use(basicAuth('master', 'edit'));
 //schedules以下に適用//
-/*
-app.all('/schedules/*', basicAuth(function(user, password) {
+app.all('/schedules', basicAuth(function(user, password) {
   return user === 'master' && password === 'edit';
-}));*/
+}));
 
 app.use('/', indexRouter);
 app.use('/schedules', scheduleRouter);
